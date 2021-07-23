21d36f504673b4d758ba57410372cd8ba3abf8fb
Tield <43399079+Tield@users.noreply.github.com>
2021-07-23 03:15:14 -0500

    Created Solutions class
    Created the Solution class using a hashmap sort consisting of a for cicle and inside it a sort of the hashmap

---
+++ Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>, hasmap{std::make_pair(nums[0],0)};
+        
+        for (size_t i = 1; i < nums.size(); i++){
+            auto it = hashmap.find(target - nums[i]);
+            if (it !== hashmap.end() && it -> second != i ) {
+                    return {it -> second, i};
+            }
+            hashmap[nums[i]] = i;
+        }
+        return {}; 
+    }
+};
